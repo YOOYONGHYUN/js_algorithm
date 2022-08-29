955d84d0f3478bc1e6236d76e6fd7407f6e418e2
YOOYONGHYUN <yyh901@naver.com>
2022-08-29 14:03:29 +0900

    Create ff.js

---
+++ ff.js
@@ -0,0 +1,54 @@
+const datas = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball",
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch",
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5",
+  },
+  {
+    category: "Electronics",
+    price: "$199.99",
+    stocked: true,
+    name: "Nexus 7",
+  },
+];
+
+const result = datas.reduce((acc, cur) => {
+  if (acc.hasOwnProperty(cur.category)) {
+    return {
+      ...acc,
+      [cur.category]: [...acc[cur.category], cur],
+    };
+  } else {
+    return {
+      ...acc,
+      [cur.category]: [cur],
+    };
+  }
+}, {});
+
+console.log(result);
